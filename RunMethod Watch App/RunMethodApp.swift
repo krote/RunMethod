7e11531871cf75ac80f93c892bbb2e41f8e57447
Tomotaka Matsushita <to-matsushita@icd.co.jp>
2023-08-14 23:00:26 +0900

    Initial Commit

---
+++ RunMethod Watch App/RunMethodApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunMethodApp.swift
+//  RunMethod Watch App
+//
+//  Created by to-matsushita on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct RunMethod_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunMethod/RunMethodApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunMethodApp.swift
+//  RunMethod
+//
+//  Created by to-matsushita on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct RunMethodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
