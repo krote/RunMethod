5621b3c47c1df07bde2a7b305fa80c0af353085e
krote <krote0924@gmail.com>
2023-09-25 23:57:21 +0900

    add

---
+++ RunMethod Watch App/VIEW/ExecExcerciseView.swift
@@ -0,0 +1,20 @@
+//
+//  ExecExcerciseView.swift
+//  RunMethod Watch App
+//
+//  Created by to-matsushita on 2023/09/03.
+//
+
+import SwiftUI
+
+struct ExecExcerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExecExcerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExecExcerciseView()
+    }
+}
